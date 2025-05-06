fdb3c9b908dcfd9e5e4612d4525fb967115dc5ae
zhennann <zhen.nann@icloud.com>
2025-05-06 15:39:31 +0800

    feat: update

--- index.ts
+++ index.ts
@@ -1,68 +1,69 @@
-import babel, { TransformOptions } from '@babel/core';
-import { Loader } from 'esbuild';
-import { createFilter, FilterPattern, Plugin } from 'vite';
+import babel, { TransformOptions } from "@babel/core";
+import { Loader } from "esbuild";
+import { createFilter, FilterPattern, Plugin } from "vite";
 
-import { esbuildPluginBabel } from './esbuildBabel';
-import { Filter, testFilter } from './filter'
+import { esbuildPluginBabel } from "./esbuildBabel.js";
+import { Filter, testFilter } from "./filter.js";
 
 export interface BabelPluginOptions {
-	apply?: Plugin['apply'];
-	enforce?: Plugin['enforce'];
-	babelConfig?: TransformOptions;
-	filter?: Filter;
-	include?: FilterPattern
-	exclude?: FilterPattern
-	loader?: Loader | ((path: string) => Loader);
+  apply?: Plugin["apply"];
+  enforce?: Plugin["enforce"];
+  babelConfig?: TransformOptions;
+  filter?: Filter;
+  include?: FilterPattern;
+  exclude?: FilterPattern;
+  loader?: Loader | ((path: string) => Loader);
 }
 
 const DEFAULT_FILTER = /\.jsx?$/;
 
 const babelPlugin = ({
-	babelConfig = {},
-	filter = DEFAULT_FILTER,
-	include,
-	exclude,
-	apply,
-	enforce = 'pre',
-	loader
+  babelConfig = {},
+  filter = DEFAULT_FILTER,
+  include,
+  exclude,
+  apply,
+  enforce = "pre",
+  loader,
 }: BabelPluginOptions = {}): Plugin => {
-	const customFilter = createFilter(include, exclude)
+  const customFilter = createFilter(include, exclude);
 
-	return {
-		name: 'babel-plugin',
+  return {
+    name: "babel-plugin",
 
-		apply,
-		enforce,
+    apply,
+    enforce,
 
-		config() {
-			return {
-				optimizeDeps: {
-					esbuildOptions: {
-						plugins: [
-							esbuildPluginBabel({
-								config: { ...babelConfig },
-								customFilter,
-								filter,
-								loader,
-							}),
-						],
-					},
-				},
-			};
-		},
+    config() {
+      return {
+        optimizeDeps: {
+          esbuildOptions: {
+            plugins: [
+              esbuildPluginBabel({
+                config: { ...babelConfig },
+                customFilter,
+                filter,
+                loader,
+              }) as any,
+            ],
+          },
+        },
+      };
+    },
 
-		transform(code, id) {
-			const shouldTransform = customFilter(id) && testFilter(filter, id);
+    transform(code, id) {
+      const shouldTransform = customFilter(id) && testFilter(filter, id);
 
-			if (!shouldTransform) return;
+      if (!shouldTransform) return;
 
-			return babel
-				.transformAsync(code, { filename: id, ...babelConfig })
-				.then((result) => ({ code: result?.code ?? '', map: result?.map }));
-		},
-	};
+      return babel
+        .transformAsync(code, { filename: id, ...babelConfig })
+        .then((result) => ({ code: result?.code ?? "", map: result?.map }));
+    },
+  };
 };
 
 export default babelPlugin;
-export * from './esbuildBabel';
-export type { Filter }
+
+export * from "./esbuildBabel.js";
+export type { Filter };
